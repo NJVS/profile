8bee1df7ffef075b1713f83af047d9bfdcf25c30
NJVS <neiljonathanv.sagisi+github@gmail.com>
2022-11-23 20:54:49 +0800

    add wavy

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
 import Wavy from './_wavy.js';
 import Carousel from './_carousel.js';
+import Typewriter from './_typewriter.js';
 import projects from './projects.json' assert { type: "json" };
 
 document.addEventListener('DOMContentLoaded', () => {
-    new Wavy(document.querySelector('#headWave'), 5, 0.005, 0.01);
+    // new Wavy(document.querySelector('#headWave'), 5, 0.005, 0.01);
     new Carousel(document.querySelector('#projectCarousel'), projects);
+
+    // wavy (wave effect)
+    document.querySelectorAll('svg[data-wavy]').forEach(wavy => {
+        wavy.style.cssText = `
+            position: absolute;
+            ${(wavy.dataset.wavy === 'top') 
+                ? 'bottom: 99%;' 
+                : 'top: 99%; rotate: 180deg;'
+            }
+            width: 100%;
+            background-color: transparent;
+            fill: var(--c-neutral-100);
+        `;
+
+        new Wavy(wavy, 5, 0.005, 0.01);
+    });
 
 
     // carousel
