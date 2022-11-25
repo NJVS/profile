7db75d0facd6a6295995b5e52f7b68f3b3f0aec3
NJVS <neiljonathanv.sagisi+github@gmail.com>
2022-11-25 21:26:40 +0800

    dropdown event

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import "../styles/main.css";
+// import "../styles/main.css";
 
 import Wavy from './_wavy.js';
 import Carousel from './_carousel.js';
@@ -6,7 +6,7 @@ import Typewriter from './_typewriter.js';
 import projects from './projects.json' assert { type: "json" };
 
 document.addEventListener('DOMContentLoaded', () => {
-    // new Wavy(document.querySelector('#headWave'), 5, 0.005, 0.01);
+    // project carousel
     new Carousel(document.querySelector('#projectCarousel'), projects);
 
     // wavy (wave effect)
@@ -29,17 +29,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const like = document.querySelector('#like');
     new Typewriter(like, like.dataset.text, like.dataset.duration);
 
-
-    // carousel
-    // let carouselIndex = 0;
-    // document.querySelector('#projectCarousel').addEventListener('click', event => {
-    //     const items = event.currentTarget.querySelectorAll('.carousel_item');
-    //     items.forEach(item => item.classList.remove('active', 'next', 'prev'));
-    //     carouselIndex = (carouselIndex == items.length - 1) ? 0 : carouselIndex + 1;
-
-    //     items[carouselIndex].classList.add('active');
-    //     items[(carouselIndex == 0) ? items.length - 1 : carouselIndex - 1].classList.add('prev');
-    //     items[(carouselIndex == items.length - 1) ? 0 : carouselIndex + 1].classList.add('next');
-    // });
+    // dropdown
+    document.querySelectorAll('.dropdown > .dropdown_trigger').forEach(trigger => {
+        trigger.addEventListener('click', event => {
+            event.preventDefault();
+            event.target.closest('.dropdown').classList.toggle('active');
+        });
+    });
 });
 
